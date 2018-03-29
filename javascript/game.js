f0736560f9a7b75d000db7d7986527ccade44163
Elizabeth <erischjanson@gmail.com>
2018-03-29 15:54:44 -0500

    cleaned up comments

--- javascript/game.js
+++ javascript/game.js
@@ -1,20 +1,21 @@
+//array of gif/button topics
 var topics=["french", "italian", "wine", "cheese", "coffee", "chocolate", "pasta", "paris", "bordeaux", "rome"];
+
+//function to create and render buttons
 function renderButton(){
 	$("#buttons").empty();
-
+	//iterate through the topics array and create buttons for each item
 for (var i=0; i < topics.length; i++){
-	var buttons=$('<button>').text(topics[i]);
-	buttons.addClass("test");
-	buttons.attr("data-topic", topics[i]);
-	$('#buttons').append(buttons);
-	
-	//assign the text as the value of an attribute "data-topic"
-};
+		var buttons=$('<button>').text(topics[i]);
+		buttons.addClass("test");
+		buttons.attr("data-topic", topics[i]);
+		$('#buttons').append(buttons);
+	};
 };
 
-renderButton();	//$('#viewGifs').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
+renderButton();
 
-	//button onclick event that will grab the gifs and apply attributes
+	//onclick event that will grab the gifs and apply attributes when a button is clicked
  $("#buttons").on("click", ".test" , function(){
 
  	var topic = ($(this).attr("data-topic"));
@@ -29,27 +30,20 @@ renderButton();	//$('#viewGifs').prepend("<img src='"+response.data[i].images.do
 		for (var i=0; i < response.data.length; i++){
 			var rating=$("<p>").text("Rating: " + response.data[i].rating);
 			var gif=$("<img>").attr("src", response.data[i].images.downsized_still.url);
-			console.log(gif);
-
-			//$('#viewGifs').prepend("<p>Rating: "+response.data[i].rating+"</p>");
-			//$('#viewGifs').prepend("<img src='"+response.data[i].images.downsized_still.url+"'>");
+			//console.log(gif);	
 			
 			gif.attr("data-state", "still");
 			gif.attr("data-still", response.data[i].images.downsized_still.url );
 			gif.attr("data-animate", response.data[i].images.downsized.url );
 			gif.addClass("gif");
-			console.log(response.data[i].images.downsized_still.url);
+			
 			$("#viewGifs").prepend(rating);
 			$("#viewGifs").prepend(gif);
-
-		}
-		
+		}		
 		});
-
  });
 
- //image onclick events that will either animate or pause the gifs
- //if click on image and state is still, flip to animate. vice versa. always reset state.
+ //onclick event that will either animate or pause the gifs depending on their data-state.
  $("#viewGifs").on("click", ".gif", function(){
  	var state=$(this).attr("data-state");
 	 	if(state==="still"){
@@ -60,17 +54,16 @@ renderButton();	//$('#viewGifs').prepend("<img src='"+response.data[i].images.do
 	 		$(this).attr("src", $(this).attr("data-still"));
 	 		$(this).attr("data-state", "still");
 
-	 	}
- 	
- })
+	 	} 	
+ });
 
+ //creates a new button with topic of user's choice
  $("#submitButton").on("click", function(event){
  	event.preventDefault();
  	
  	var userInput=$("#userInput").val();
  	
- 	topics.push(userInput);
- 	console.log(topics);
+ 	topics.push(userInput); 	
  	renderButton();
- })
+ });
 
